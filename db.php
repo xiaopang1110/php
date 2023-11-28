fae2317851a0e03824bf160e384d691fd523015d
‘xiaopang1110’ <z17661195396@gmail.com>
2023-11-28 17:19:16 +0800

    22

--- db.php
+++ db.php
@@ -1,14 +1,28 @@
 <?
-$servername = "10.194.0.2:3306";
-$username = "root";
-$password = "mysql@ime";
- 
-// 创建连接
-$conn = new mysqli($servername, $username, $password);
- 
-// 检测连接
-if ($conn->connect_error) {
-    die("连接失败: " . $conn->connect_error);
-} 
-echo "连接成功";
+$mysql_conf = array(
+    'host'    => '10.194.0.2:3306', 
+    'db'      => 'wordpress', 
+    'db_user' => 'root', 
+    'db_pwd'  => 'mysql@ime', 
+    );
+
+$mysqli = @new mysqli($mysql_conf['host'], $mysql_conf['db_user'], $mysql_conf['db_pwd']);
+if ($mysqli->connect_errno) {
+    die("could not connect to the database:\n" . $mysqli->connect_error);//诊断连接错误
+}
+$mysqli->query("set names 'utf8';");//编码转化
+$select_db = $mysqli->select_db($mysql_conf['db']);
+if (!$select_db) {
+    die("could not connect to the db:\n" .  $mysqli->error);
+}$sql = "select count(*) from wp_users;";
+$res = $mysqli->query($sql);
+if (!$res) {
+    die("sql error:\n" . $mysqli->error);
+}
+ while ($row = $res->fetch_assoc()) {
+        var_dump($row);
+    }
+
+$res->free();
+$mysqli->close();
 ?>
