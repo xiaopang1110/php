854706aee85b7a4c8724c8c9b97da1bb9003fbb9
‘xiaopang1110’ <z17661195396@gmail.com>
2023-11-28 18:25:28 +0800

    27

--- db.php
+++ db.php
@@ -19,6 +19,27 @@
         echo "<h1>MySQL 连接失败: " . $conn->connect_error . "</h1>";
     } else {
         echo "<h1>MySQL 连接成功！</h1>";
+
+        // 执行查询语句
+        $query = "SELECT * FROM wp_users LIMIT 1";
+        $result = $conn->query($query);
+
+        // 检查查询结果
+        if ($result) {
+            // 提取查询结果中的数据
+            $row = $result->fetch_assoc();
+
+            // 在 HTML 中展示数据
+            echo "<h2>查询结果：</h2>";
+            echo "<p>user_nicename: " . $row['user_nicename'] . "</p>";
+            echo "<p>user_login: " . $row['user_login'] . "</p>";
+            // 继续根据您的表结构添加更多字段
+
+            // 释放查询结果资源
+            $result->free();
+        } else {
+            echo "<h2>查询失败！</h2>";
+        }
     }
     ?>
     <?php
